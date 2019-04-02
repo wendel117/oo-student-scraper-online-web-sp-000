15efc3f922b6a5039cb2da599ac6af8de8545156
Wendy Hamilton <wdhamilton117@gmail.com>
2019-04-02 14:33:58 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,31 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css('.student-card a').each do |student|
+      students << { name: student.css('.student-name').text, location: student.css('.student-location').text, profile_url: student.attr('href') }
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    student = {}
+    doc = Nokogiri::HTML(open(profile_url))
+    social_links = doc.css('.social-icon-container a').map { |x| x.attr('href') }
+    social_links.each do |i|
+      if i.include?('twitter')
+        student[:twitter] = i
+      elsif i.include?('linkedin')
+        student[:linkedin] = i
+      elsif i.include?('github')
+        student[:github] = i
+      else
+        student[:blog] = i
+      end
+    end
+    student[:profile_quote] = doc.css('.profile-quote').text if doc.css('.profile-quote').text
+    student[:bio] = doc.css('.bio-content div p').text if doc.css('.bio-content div p').text
+    student
   end
-
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,23 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
-  def initialize(student_hash)
-    
-  end
+    def initialize(student_hash)
+      student_hash.each { |k,v| self.send("#{k}=", v)}
+      @@all << self
+    end
 
-  def self.create_from_collection(students_array)
-    
-  end
+    def self.create_from_collection(students_array)
+      students_array.each { |i| self.new(i) }
+    end
 
-  def add_student_attributes(attributes_hash)
-    
-  end
+    def add_student_attributes(attributes_hash)
+      attributes_hash.each { |k,v| self.send("#{k}=", v)}
+    end
 
-  def self.all
-    
+    def self.all
+      @@all
+    end
   end
-end
-
